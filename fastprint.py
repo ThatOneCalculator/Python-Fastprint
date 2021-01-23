203f939dbec81cad5408c8c9d8a814e4647a8e46
Kainoa Kanter <44733677+ThatOneCalculator@users.noreply.github.com>
2021-01-22 19:12:53 -0800

    Update fastprint.py

--- fastprint.py
+++ fastprint.py
@@ -3,31 +3,4 @@ def pr(st, delay=1):
 	for line in st.splitlines():
 	    print(line)
 	    time.sleep(delay)
-st = """This is
-the first
-very long
-string"""
-pr(st)
-st="""
-This is
-the SECOND
-one, ok?\n
-coolio.\n"""
-pr(st)
-st="""\n\nThis was made by
-Kainoa Kanter
-on 2/13/19"""
-pr(st)
-st="\nIt also\nkinda works with\nsingle quotes\nbut that kinda defeats\nthe purpose"
-pr(st)
-st="""\nYou can also change the
-print time!
-This is at 0.3
-instead of
-1.0"""
-pr(st, 0.3)
-st="""\nCatch me
-on Github!
-My username is
-ThatOneCalculator"""
-pr(st)
+
